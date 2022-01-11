381b1f7a1e711533f5fe183937b9171e8a0701e3
Aeolus <879687922@qq.com>
2022-01-12 01:59:05 +0800

    final version

---
+++ ga-algo.py
@@ -0,0 +1,79 @@
+import torch
+
+from util import *
+
+class GAImageSeg:
+
+    '''
+        A two-class implementation of 
+        https://www.researchgate.net/publication/339595927_Multi-Thresholding_Image_Segmentation_Using_Genetic_Algorithm
+    '''
+
+    def __init__(self, cross_prob=0.95, mute_prob=0.05) -> None:
+        self.threshes = [torch.randint(0, 256, (1, 1))[0][0], torch.randint(0, 256, (1, 1))[0][0]]
+        
+        self.cross_prob = cross_prob
+        self.mute_prob = mute_prob
+
+    @staticmethod
+    def cal_fit(img, threshes):
+        return [cal_fitness(img, i) for i in threshes]
+
+    def crossover(self, thresh1, thresh2):
+        bin1, bin2 = dec2bin(thresh1), dec2bin(thresh2)
+        if len(bin1) < 2 or len(bin2) < 2:
+            return bin1, bin2 
+        cross_pos = torch.randint(1, len(bin1), (1, 1))[0][0]
+        bin1 = bin1[:cross_pos] + bin2[cross_pos:]
+        bin2 = bin2[:cross_pos] + bin1[cross_pos:]
+        return bin2dec(bin1), bin2dec(bin2)
+    
+    def mutation(self, thresh):
+        bin = dec2bin(thresh)
+        mute_pos = torch.randint(len(bin), (1, 1))[0][0]
+        bin[mute_pos] = '0' if bin[mute_pos] == '1' else '0'
+        return bin2dec(bin)
+
+    def iter(self, img, times, max_child=10):
+        self.fits = self.cal_fit(img, self.threshes)
+        for _ in range(times):
+            new_threshes = []
+            new_fitnesses = []
+            cnt = 0
+            while len(self.threshes) > len(new_threshes) and cnt < max_child:
+                thresh1, thresh2 = rolletwheel(self.threshes, self.cal_fit(img, self.threshes))
+                fits = self.cal_fit(img, [thresh1, thresh2])
+                cnt += 1
+                if torch.rand(1) < self.cross_prob:
+                    thresh1, thresh2 = self.crossover(thresh1, thresh2)
+                if torch.rand(1) < self.mute_prob:
+                    thresh1 = self.mutation(thresh1)
+                if torch.rand(1) < self.mute_prob:
+                    thresh2 = self.mutation(thresh2)
+                for thresh, fit in zip([thresh1, thresh2], self.cal_fit(img, [thresh1, thresh2])):
+                    if fit > max(fits):
+                        new_threshes.append(thresh)
+                        new_fitnesses.append(fit)
+            if cnt == max_child:
+                continue
+            self.threshes = new_threshes
+            self.fits = new_fitnesses
+        pairs = [(i, j) for i, j in zip(self.threshes, self.fits)]
+        
+        return sorted(pairs, key=lambda x:x[1])[-1]
+
+if __name__ == '__main__':
+    import cv2 as cv
+    import numpy as np
+
+    img  = cv.imread('./fisherman.jpg', cv.IMREAD_GRAYSCALE)
+    img = torch.from_numpy(img).float()
+    ga = GAImageSeg()
+    print('initial threshes are {}, {}'.format(*ga.threshes))
+    thresh, fit = ga.iter(img, 100)
+    print(f'thresh is {thresh}, fit is {fit}')
+    mask = img > thresh
+    img = 255 * mask.int().numpy().astype(np.uint8)
+    b = cv.imshow('hey', img)
+    cv.waitKey()
+
--- util.py
+++ util.py
@@ -1,13 +1,55 @@
 import torch
 
-def calculate_diff(img, thresh):
-    mask1 = img > thresh
-    mask2 = img <= thresh
-    inner_std1 = torch.std(img[mask1])
-    inner_std2 = torch.std(img[mask2])
-    between_dif = (torch.mean(img[mask1]) - torch.mean(img[mask2])) ** 2
-    return (inner_std1 + inner_std2) / between_dif
+def cal_fitness(img, thresh):
+    mask1 = img < thresh
+    mask2 = img >= thresh
+    # 计算各类别数量 -> hist(x)
+    num1 = torch.sum(mask1, dtype=torch.float32) + 1e-3
+    num2 = torch.sum(mask2, dtype=torch.float32) + 1e-3
+    # 计算各类别的均值
+    mean1 = torch.sum(img[mask1]) / num1
+    mean2 = torch.sum(img[mask2]) / num2
+    p1 = num1 / (num1 + num2)
+    p2 = 1 - p1
+    # 加权计算全局均值
+    mean_global = p1 * mean1 + p2 * mean2
+    # 计算类间方差
+    Sbetween = p1 * (mean1 - mean_global) ** 2 + p2 * (mean2 - mean_global) ** 2
+    # 计算类内方差
+    S1 = torch.sum((img[mask1] - mean1) ** 2)
+    S2 = torch.sum((img[mask2] - mean2) ** 2)
+    Si = S1 + S2
+    Sg = torch.sum((img - mean_global) ** 2)
+    Swithin = Si / Sg
+    return Sbetween / Swithin
 
-def cal_fitness(img, threshes, L=256):
-    L = torch.log2(L)
-    n = len(threshes)
+
+def rolletwheel(threshes, fits):
+    summ = float(sum(fits))
+    # 将所有适应度进行归一化
+    norm_fits = [i / summ for i in fits]
+    accu_probs = [0]
+    # 计算累积概率
+    for prob in norm_fits:
+        accu_probs.append(accu_probs[-1] + prob)
+    accu_probs.pop(0)
+    # 生成随机值，由于该算法每次仅生成两个阈值，为此仅生成两个随机值用于计算
+    chosen_probs = torch.rand(2)
+    chosen_thresh = []
+    for prob in chosen_probs:
+        for cnt, accu_prob in enumerate(accu_probs):
+            if accu_prob > prob:
+                chosen_thresh.append(threshes[cnt])
+                break
+    return chosen_thresh
+
+
+def bin2dec(bin):
+    binstring = ''.join([i for i in bin])
+    return int(binstring, 2)
+
+def dec2bin(dec):
+    return [i for i in str(bin(dec))[2:]]
+
+if __name__ == '__main__':
+    rolletwheel([8, 9], [7, 100])
