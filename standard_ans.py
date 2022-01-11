f5c7bf74889925450f60b1cd70ce227f8e16a045
Aeolus <879687922@qq.com>
2022-01-12 01:59:58 +0800

    forget the standard_ans

---
+++ standard_ans.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+import numpy as np
+from util import *
+import matplotlib.pyplot as plt
+
+# img_path = './fisherman.jpg'
+img_path = './lena.jpg'
+
+img  = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
+img = torch.from_numpy(img).float()
+fits = [(thresh, cal_fitness(img, thresh)) for thresh in range(256)]
+# show the change of fitness
+plt.plot([i[0] for i in fits], [i[1]for i in fits])
+plt.show()
+best_fit = sorted(fits, key=lambda x:x[1])[-1]
+print(f'best_fit is {best_fit}')
+mask = img > best_fit[0]
+img = 255 * mask.int().numpy().astype(np.uint8)
+b = cv.imshow('hey', img)
+cv.waitKey()
